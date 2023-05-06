1f0f92a16f8dc28f96fb2779c4f65fa90c6c5687
adsoftsito <adsoft@live.com.mx>
2023-05-06 03:13:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: ['my-kafka-deployment-67c4cc5858-26mhd:9092']
+  brokers: ['https://my-kafka-service-kafka-adsoftsito.cloud.okteto.net:9092']
 });
                                                        // my-kafka-deployment-67c4cc5858-26mhd
 const producer = kafka.producer()
