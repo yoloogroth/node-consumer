2428ff6e23a18e79551dad020e992ceedf9a03f0
Yoloogroth <zS20020311@estudiantes.uv.mx>
2023-05-29 04:38:11 -0600

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-  res.send('kafka api - adsoft');
+  res.send('kafka api - yolo');
 });
 
 const run = async (username) => {
@@ -42,6 +42,57 @@ app.get('/like', (req, res, next) => {
   run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
 });
+//node - topic reaction - uId=userId, oId=objectId, rId=reactionId
+const ruun = async (uId, oId, rId) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'reactions',
+    messages: [ 
+{ 
+  'value': `{ "userId": "${uId}",  "objectId": "${oId}", "reactionId": "${rId}"}` 
+  } 
+    ],
+  })
+ await producer.disconnect()
+}
+
+app.get('/reaction', (req, res, next) => {
+const uId = req.query.userId;
+const oId = req.query.objectId;
+const rId = req.query.reactionId;
+res.send({'userId:': uId, 'objectId': oId,'reactionId' : rId } );
+ruun(uId, oId, rId).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+//node - topic comments    uId=userId, oId=objectId, comment=message
+const r = async (uId, oId, comment) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'comments',
+    messages: [ 
+  { 
+    'value': `{ "userId": "${uId}",  "objectId": "${oId}", "comment": "${comment}"}`
+  } 
+    ],
+  })
+ await producer.disconnect()
+}
+
+app.get('/comments', (req, res, next) => {
+const uId = req.query.userId;
+const oId = req.query.objectId;
+const comment = req.query.comment;
+res.send({'userId:': uId, 'objectId': oId,'comment' : comment} );
+r(uId, oId, comment).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
